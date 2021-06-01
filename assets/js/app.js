43a37b7dae8983701a3f23aa70d5cec6f3a99bf7
brittanyaylia <whitakerbrittany18@gmail.com>
2021-06-01 16:38:20 -0700

    app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-// @TODO: YOUR CODE HERE!
+// D3 
+
+// chart code - resize the chart automatically
+function makeResponsive() {
+
+    // if SVG area is not empty - replace with resized chart 
+    var svgArea = d3.select("body").select("svg");
+  
+    // clear SVG
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+    
+    // chart setup 
+    var svgWidth = 980;
+    var svgHeight = 600;
+  
+    // SVG margins
+    var margin = {
+      top: 20,
+      right: 40,
+      bottom: 90,
+      left: 100
+    };
